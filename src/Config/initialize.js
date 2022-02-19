12c414cd5056680d04e0b895a80d527dc5f92b6f
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-02-18 19:46:35 -0500

    config firebase

---
+++ src/Config/initialize.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmhQWfI2sxgl8aZAQhrebp_K4lMNVPL5k",
+  authDomain: "woow-app-project.firebaseapp.com",
+  projectId: "woow-app-project",
+  storageBucket: "woow-app-project.appspot.com",
+  messagingSenderId: "926967780191",
+  appId: "1:926967780191:web:f15f10821fe51710f79cea",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
