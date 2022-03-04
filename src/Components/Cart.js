ecf47df435fde615d1b297451dc56b7a8c1a7091
Lucero <lucerogoga63@hotmail.com>
2022-03-04 13:54:20 -0500

    fix bug about counter

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCart } from "../Components/Context/CartContext";
 import ProductAddedCart from "./ProductAddedCart";
 import Title from "./Title";
 
 const Cart = () => {
+  const [clientName, setClientName] = useState("");
+  const [mesaNumber, setMesaNumber] = useState("");
   const { cart } = useCart();
   console.log(cart);
 
   return (
     <>
       <Title title="Order" quantity={cart.length} />
+      <input
+        id="client"
+        className="client--input"
+        placeholder="Client Name"
+        onChange={(ev) => setClientName(ev.target.value)}
+      ></input>
       <div>
         {cart.map((cartProduct) => (
           <ProductAddedCart
