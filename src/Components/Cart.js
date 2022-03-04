6bc4e4eb06e69e55077ec071e85db169b9c995a2
Lucero <lucerogoga63@hotmail.com>
2022-03-04 14:37:48 -0500

    add styles to Cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,29 +2,44 @@ import React, { useState } from "react";
 import { useCart } from "../Components/Context/CartContext";
 import ProductAddedCart from "./ProductAddedCart";
 import Title from "./Title";
-
+import "../Assets/Cart.css";
 const Cart = () => {
   const [clientName, setClientName] = useState("");
-  const [mesaNumber, setMesaNumber] = useState("");
+  const [tableNumber, setTableNumber] = useState("");
   const { cart } = useCart();
   console.log(cart);
 
   return (
     <>
-      <Title title="Order" quantity={cart.length} />
-      <input
-        id="client"
-        className="client--input"
-        placeholder="Client Name"
-        onChange={(ev) => setClientName(ev.target.value)}
-      ></input>
-      <div>
-        {cart.map((cartProduct) => (
-          <ProductAddedCart
-            cartProduct={cartProduct}
-            key={cartProduct.idChanges}
-          />
-        ))}
+      <div className="cart-content">
+        <Title title="Order" quantity={cart.length} />
+        <div className="client-info--content">
+          <input
+            type="text"
+            id="client"
+            className="client--input"
+            placeholder="Client Name"
+            onChange={(ev) => setClientName(ev.target.value)}
+          ></input>
+          <select
+            className="table--input"
+            placeholder="Nº Table"
+            name="Nº Table"
+            id="table"
+          >
+            <option value="Table 1">Table 1</option>
+            <option value="Table 2">Table 2</option>
+            <option value="Table 3">Table 3</option>
+          </select>
+        </div>
+        <div>
+          {cart.map((cartProduct) => (
+            <ProductAddedCart
+              cartProduct={cartProduct}
+              key={cartProduct.idChanges}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
