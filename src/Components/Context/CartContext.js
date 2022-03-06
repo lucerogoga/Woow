c4541643e11e3916e8204acfbb4437cfd6738795
Lucero <lucerogoga63@hotmail.com>
2022-03-05 19:01:16 -0500

    cleaning cartContext

--- src/Components/Context/CartContext.js
+++ src/Components/Context/CartContext.js
@@ -1,14 +1,12 @@
-import React,{useState, useContext, createContext} from "react"
+import React, { useState, useContext, createContext } from "react";
 
 const Cart = createContext();
 
-const CartContext = ({children}) => {
+const CartContext = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  return <Cart.Provider value={{ cart, setCart }}>{children}</Cart.Provider>;
+};
 
-    const [cart, setCart ] = useState([]);
-    const [idDetail, setIdDetail] = useState(1);
-    return <Cart.Provider value={{cart, setCart, idDetail, setIdDetail}}>{children}</Cart.Provider>
-} 
-
-export default CartContext
+export default CartContext;
 
 export const useCart = () => useContext(Cart);
