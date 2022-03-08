bd64bee85d1c035d9a0933f2b0bdbe0d27f6ec32
Lucero <lucerogoga63@hotmail.com>
2022-03-08 12:42:44 -0500

    add chefView

---
+++ src/Pages/Chef/ChefView.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import { useAuth } from "../../Components/Context/AuthContext";
+import "../../Assets/Navbar.css";
+import "../../Assets/Sidebar.css";
+import "../../Assets/OrderCard.css";
+
+import NavBarChef from "../../Components/NavBarChef";
+import SideBarChef from "../../Components/SideBarChef";
+
+const AdminView = () => {
+  const [open, setOpen] = useState(false);
+  const { logout } = useAuth();
+  let location = useLocation();
+  const { pathname } = location;
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  return (
+    <>
+      <NavBarChef onClickMenu={() => setOpen(!open)} currentPath={pathname} />
+      <div>
+        {open && (
+          <SideBarChef
+            onClose={(e) => setOpen(false)}
+            onClickLogout={handleLogout}
+          />
+        )}
+        <div className="content">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminView;
