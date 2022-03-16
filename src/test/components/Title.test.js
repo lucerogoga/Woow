23f8126a2275511fbc608fa269e8d7f44f98c8a6
Lucero <lucerogoga63@hotmail.com>
2022-03-15 21:42:28 -0500

    fixing test

---
+++ src/test/components/Title.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import Title from "../../Components/Title";
+
+// describe("render content", () => {
+//   //   const valores = {
+//   //     title: "Titulo de prueba",
+//   //     quantity: 3,
+//   //   };
+
+//   const title = "ayuda";
+//   const qty = 3;
+
+//   const view = render(<Title title={title} quantity={qty} />);
+//   //   const componentito = render(<Title  title={title} quantity={qty}/>);
+//   //   const componentito = render(<Title  title={valores.title} quantity={valores.quantity}/>);
+
+//   console.log(view);
+
+//   expect(view.container).toHaveTextContent(title);
+// });
+test("render content", () => {
+  //   const valores = {
+  //     title: "Titulo de prueba",
+  //     quantity: 3,
+  //   };
+
+  const title = "ayuda";
+  const qty = 3;
+
+  const view = render(<Title title={title} quantity={qty} />);
+  //   const componentito = render(<Title  title={title} quantity={qty}/>);
+  //   const componentito = render(<Title  title={valores.title} quantity={valores.quantity}/>);
+  console.log(view);
+});
