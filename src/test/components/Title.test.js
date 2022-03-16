b097066e0478f36926db4a26ca9a99cb3b64db76
Lucero <lucerogoga63@hotmail.com>
2022-03-16 12:03:01 -0500

    testing WIP

--- src/test/components/Title.test.js
+++ src/test/components/Title.test.js
@@ -1,36 +1,14 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Title from "../../Components/Title";
 
-// describe("render content", () => {
-//   //   const valores = {
-//   //     title: "Titulo de prueba",
-//   //     quantity: 3,
-//   //   };
-
-//   const title = "ayuda";
-//   const qty = 3;
-
-//   const view = render(<Title title={title} quantity={qty} />);
-//   //   const componentito = render(<Title  title={title} quantity={qty}/>);
-//   //   const componentito = render(<Title  title={valores.title} quantity={valores.quantity}/>);
-
-//   console.log(view);
-
-//   expect(view.container).toHaveTextContent(title);
-// });
-test("render content", () => {
-  //   const valores = {
-  //     title: "Titulo de prueba",
-  //     quantity: 3,
-  //   };
-
-  const title = "ayuda";
+test("render content from Title Component", () => {
+  const title = "Title";
   const qty = 3;
 
-  const view = render(<Title title={title} quantity={qty} />);
-  //   const componentito = render(<Title  title={title} quantity={qty}/>);
-  //   const componentito = render(<Title  title={valores.title} quantity={valores.quantity}/>);
-  console.log(view);
+  const { getByTestId } = render(<Title title={title} quantity={qty} />);
+
+  const text = screen.getByTestId("title-text");
+  expect(text).toHaveTextContent(title);
 });
