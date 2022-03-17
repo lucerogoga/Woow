7176b84a083483ce14f942bc3cc165c1ece0ae0a
Walter Rivas <rivasw96@hotmail.com>
2022-03-17 09:10:50 -0500

    creating clock with moment js wip

--- src/Components/Time.js
+++ src/Components/Time.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../Assets/Time.css";
 import { ReactComponent as Clock } from "../Assets/icons/clock.svg";
 import moment from "moment";
@@ -9,30 +9,57 @@ const Time = ({ start, end }) => {
   // .toDate()
   // console.log('time component , ', start)
   // console.log("time component , ", start.toDate());
+  const [time, setTime] = useState('')
+
+  const current = moment();
+
+
+  const handleDiffTime = () => {
+    const a = moment("2022-03-10 13:31:12");
+    // const b = moment("2022-03-11 15:31:15");
+  
+    // ! el actual!
+  
+    const difference = moment.preciseDiff(a, current, true);
+    // const difference = moment.preciseDiff(a, b, true);
+  
+    let {hours, minutes, seconds} = difference;
+  
+    seconds = seconds < 10 ? "0" + seconds : seconds
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    hours = hours < 10 ? "0" + hours : hours
+    console.log(hours,':',minutes,':',seconds)
+  
+    // return hours +':' + minutes + ':' + seconds
+    const timeDiff = hours +':' + minutes + ':' + seconds
+    setTime(timeDiff);
+  }
+  useEffect(() => {
+    
+    handleDiffTime()
+    // setInterval(handleDiffTime, 10000)
+    // setInterval(handleDiffTime, 10000)
+    // return time
+  }, [time, current])
   // const diff = () => {
-  const a = moment("2022-03-10 13:31:12");
-  const b = moment("2022-03-11 15:31:15");
 
-  // ! el actual!
-  // const current = moment();
+    
 
-  const diferenciaa = moment.preciseDiff(a, b, true);
-  // const diferencia = JSON.stringify(moment.preciseDiff(a, b, true));
-  // var duration = diferenciaa(diferenciaa, "HH:mm:ss a");
-  // console.log("salvame diferencia formato, ", duration);
-  console.log("salvame diferencia json, ", diferenciaa);
+  // }
 
-  const {hours} = diferenciaa;
-  // const {hours, minutes, seconds} = diferenciaa;
-  // console.log('horas: ', hours, ' - minutos: ', minutes, ' - segundos: ' , seconds)
-  console.log('horas: ', hours)
+ 
+  
+   
+  // console.log('el tiempo es con function: ', diff)
+  console.log('el tiempo es: ', time)
+  // console.log('el tiempo es: ', timeDiff)
   // !-----
 
   return (
     <div className="order-cart--containertime">
       <Clock className="order-cart--clock" width={16} height={16} />
-      <h3 className="order-cart--minutes">{"00:00:00"}</h3>
-      {/* <h3 className="order-cart--minutes">{duration}</h3> */}
+      {/* <h3 className="order-cart--minutes">{"00:00:00"}</h3> */}
+      <h3 className="order-cart--minutes">{time}</h3>
     </div>
   );
 };
