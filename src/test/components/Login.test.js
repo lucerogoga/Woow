2767c72bc511148d91a2298b50c55c6b5b713aaf
Lucero <lucerogoga63@hotmail.com>
2022-03-17 13:50:53 -0500

    login test success

--- src/test/components/Login.test.js
+++ src/test/components/Login.test.js
@@ -2,17 +2,41 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Login from "../../Components/Login";
+import {
+  AuthProvider,
+  AuthContext,
+  useAuth,
+} from "../../Components/Context/AuthContext";
 
+// jest.mock("../../Components/Context/AuthContext");
+// import mount from "enzyme";
+// import { useAuth } from "../../Components/Context/AuthContext";
+// import AuthProvider from "../../Components/Context/AuthContext";
+// import { authContext } from "../../Components/Context/AuthContext";
+// import { AuthProvider } from "../Comp/Context/AuthContext";
 // pay attention to write it at the top level of your file
 const mockedUsedNavigate = jest.fn();
-
-jest.mock('react-router-dom', () => ({
-   ...jest.requireActual('react-router-dom') as any,
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockedUsedNavigate,
 }));
 
+// jest.mock("../../Components/Context/AuthContext", () => ({
+//   ...jest.requireActual("../../Components/Context/AuthContext"),
+//   useAuth: jest.fn(() => ({
+//     login: () => true,
+//   })),
+// }));
+
+jest.mock("../../Components/Context/AuthContext", () => ({
+  useAuth: () => ({
+    login: () => true,
+    user: "useruid",
+  }),
+}));
+
 describe("Login", () => {
-  test("render content from Title Component", () => {
+  test("render content from Login Component", () => {
     const { getByPlaceholderText } = render(<Login />);
 
     const inputEmail = screen.getByPlaceholderText("User email");
@@ -23,9 +47,14 @@ describe("Login", () => {
 
     expect(screen.getByDisplayValue("waiter@gamail.com")).toBeInTheDocument();
     expect(screen.getByDisplayValue("123456")).toBeInTheDocument();
-    //   screen.getByText(title);
-    // const text = screen.getByTestId("title-text");
-    //   expect(view.container).toHaveTextContent();
-    //   expect(view.container).toHaveTextContent();
   });
 });
+
+// test('component handles button click', () => {
+//     const {getByText} = render(
+//       <MyComponent />
+//     )
+//     const button = getByText('Click me')
+//     fireEvent.click(button)
+//     expect...?
+//   }
