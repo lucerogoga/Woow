53351f835ae9cfa8608435d8bd3c9a861ab7c243
Walter Rivas <rivasw96@hotmail.com>
2022-03-17 13:14:42 -0500

    try to create test in login

--- src/test/components/Login.test.js
+++ src/test/components/Login.test.js
@@ -2,17 +2,20 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Login from "../../Components/Login";
+import { authContext, AuthProvider, useAuth} from "../../Components/Context/AuthContext";
+
 
 // pay attention to write it at the top level of your file
 const mockedUsedNavigate = jest.fn();
 
 jest.mock('react-router-dom', () => ({
-   ...jest.requireActual('react-router-dom') as any,
+   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate,
 }));
 
 describe("Login", () => {
   test("render content from Title Component", () => {
+   
     const { getByPlaceholderText } = render(<Login />);
 
     const inputEmail = screen.getByPlaceholderText("User email");
@@ -23,9 +26,32 @@ describe("Login", () => {
 
     expect(screen.getByDisplayValue("waiter@gamail.com")).toBeInTheDocument();
     expect(screen.getByDisplayValue("123456")).toBeInTheDocument();
+
+    // -----------
+    const wrapper = render(
+      <AuthProvider>
+        <Login />
+      </AuthProvider>
+    )
+
+    const input = wrapper.find('input')
+
+    input.simulate('change')
+      // expect(wrapper.find('input'))
+      
     //   screen.getByText(title);
     // const text = screen.getByTestId("title-text");
     //   expect(view.container).toHaveTextContent();
     //   expect(view.container).toHaveTextContent();
   });
 });
+
+
+// test('component handles button click', () => {
+//     const {getByText} = render(
+//       <MyComponent />
+//     )
+//     const button = getByText('Click me')
+//     fireEvent.click(button)
+//     expect...?
+//   }
