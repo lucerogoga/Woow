b380135796a6d41fe89472f32c13d4525ab52537
Lucero <lucerogoga63@hotmail.com>
2022-03-17 11:11:05 -0500

    TEST WIP

--- src/test/components/Login.test.js
+++ src/test/components/Login.test.js
@@ -1,6 +1,31 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Login from "../../Components/Login";
 
-test();
+// pay attention to write it at the top level of your file
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom') as any,
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+describe("Login", () => {
+  test("render content from Title Component", () => {
+    const { getByPlaceholderText } = render(<Login />);
+
+    const inputEmail = screen.getByPlaceholderText("User email");
+    const inputPassword = screen.getByPlaceholderText("User password");
+
+    fireEvent.change(inputEmail, { target: { value: "waiter@gamail.com" } });
+    fireEvent.change(inputPassword, { target: { value: "123456" } });
+
+    expect(screen.getByDisplayValue("waiter@gamail.com")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("123456")).toBeInTheDocument();
+    //   screen.getByText(title);
+    // const text = screen.getByTestId("title-text");
+    //   expect(view.container).toHaveTextContent();
+    //   expect(view.container).toHaveTextContent();
+  });
+});
