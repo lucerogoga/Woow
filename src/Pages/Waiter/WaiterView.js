4ec6bb906be2c96a4e414e73da8cb4a67231cdaf
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-03-20 18:30:22 -0500

    clean code in all sidebars components

--- src/Pages/Waiter/WaiterView.js
+++ src/Pages/Waiter/WaiterView.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-
 import { useAuth } from "../../Components/Context/AuthContext";
 import { useSideBarCart } from "../../Components/Context/SideBarCartContext";
 import "../../Assets/Navbar.css";
 import "../../Assets/Sidebar.css";
 import "../../Assets/OrderCard.css";
-
 import NavBarWaiter from "../../Components/NavBarWaiter";
 import SideBarWaiter from "../../Components/SideBarWaiter";
 import SideBarCart from "../../Components/SideBarCart";
@@ -20,8 +18,6 @@ export const WaiterView = () => {
   const handleSidebarClose = () => {
     setOpen(false);
   };
-
-  // const { isSideBarCartOpen, setIsSideBarCartOpen } = useSideBarCart();
 
   const { openCart, setOpenCart } = useSideBarCart();
 
@@ -47,11 +43,8 @@ export const WaiterView = () => {
         onClickSideBar={handleSidebarCartOpen}
         currentPath={pathname}
         onClickLogout={handleLogout}
-        // isActive={}
       />
       <div>
-        {/* <SideBarCart onClose={(e) => setIsSideBarCartOpen(false)} /> */}
-
         <SideBarCart isOpen={openCart} onClose={handleSidebarCartClose} />
         <SideBarWaiter
           isOpen={open}
