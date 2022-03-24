ca44c3c07d451fc924b81e8a75321df8fc03f8cc
Lucero <lucerogoga63@hotmail.com>
2022-03-24 09:19:43 -0500

    test Wip

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,12 +2,12 @@
 import "../Assets/Login.css";
 import logo from "../Assets/icons/logo-rotate.svg";
 import Error from "./Error";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useAuth } from "./Context/AuthContext";
 import { getUser } from "../Services/FirestoreServices";
 // import { useRol } from "./Context/RolContex";
-import ActionButton from "./ActionButton";
+
 export const Login = () => {
   const navigate = useNavigate();
   const [loginEmail, setLoginEmail] = useState("");
