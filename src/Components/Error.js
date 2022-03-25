3dfbc1a6f5ab73b54cf4c78efbeac45ec203add6
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-03-24 19:51:16 -0500

    Handling errors in Error Component

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,21 +2,17 @@ import React, { useState } from "react";
 import "../Assets/Error.css";
 import { ReactComponent as X } from "../Assets/icons/x.svg";
 
-const Error = ({ message, onClose }) => {
-  // const toggle = () => {
-  //   setCloseX(!closeX);
-  // };
-
+const Error = ({ message, onClose, isVisible }) => {
   return (
     <>
-      <div className="err-msg">
-        <div className="err-msg-header">
-          <span onClick={onClose}>
+      <div className={"err-msg " + (isVisible ? "" : "hide")}>
+        <div className="err-msg__content">
+          <span className="err-msg__close-icon" onClick={onClose}>
             <X className="err-msg-x" />
           </span>
-        </div>
 
-        <h1 className="err-msg--text">{message}</h1>
+          <h1 className="err-msg--text">{message}</h1>
+        </div>
       </div>
     </>
   );
