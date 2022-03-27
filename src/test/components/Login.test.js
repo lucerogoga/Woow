205d77e4265a5129d07556887e8ab5c3b0a1b2d9
Lucero <lucerogoga63@hotmail.com>
2022-03-27 12:40:24 -0500

    test wip

--- src/test/components/Login.test.js
+++ src/test/components/Login.test.js
@@ -1,19 +1,9 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import {
-  render,
-  screen,
-  fireEvent,
-  act,
-  waitFor,
-} from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 import Login from "../../Components/Login";
-import {
-  AuthProvider,
-  AuthContext,
-  useAuth,
-} from "../../Components/Context/AuthContext";
 
 // pay attention to write it at the top level of your file
 const mockedUsedNavigate = jest.fn();
@@ -23,37 +13,96 @@ jest.mock("react-router-dom", () => ({
 }));
 
 // !--------Solicitar explicacion de este mock si esta bien o no?
+const mockedLogin = jest.fn();
+
 jest.mock("../../Components/Context/AuthContext", () => ({
   useAuth: () => ({
-    login: () => true,
+    login: mockedLogin,
     user: "useruid",
   }),
 }));
 
+const testEmailValid = "waiter@gamail.com";
+const testEmailInvalid = "badformatcom";
+const testPasswordValid = "12345678";
+function fillLoginFormWithValidData(email, password) {
+  const inputEmail = screen.getByPlaceholderText("User email");
+  const inputPassword = screen.getByPlaceholderText("User password");
+
+  fireEvent.change(inputEmail, { target: { value: email } });
+  fireEvent.change(inputPassword, { target: { value: password } });
+}
+
 describe("Login", () => {
-  test("render content from Login Component", () => {
+  test("render content from Login Component", async () => {
     const { getByPlaceholderText } = render(<Login />);
 
-    const inputEmail = screen.getByPlaceholderText("User email");
-    const inputPassword = screen.getByPlaceholderText("User password");
-
-    fireEvent.change(inputEmail, { target: { value: "waiter@gamail.com" } });
-    fireEvent.change(inputPassword, { target: { value: "123456" } });
-
-    expect(screen.getByDisplayValue("waiter@gamail.com")).toBeInTheDocument();
-    expect(screen.getByDisplayValue("123456")).toBeInTheDocument();
-  });
-
-  test("click button Login from Login Component", async () => {
-    const { getByText } = render(<Login />);
-
-    const handleSubmit = jest.fn();
+    fillLoginFormWithValidData(testEmailValid, testPasswordValid);
+    expect(screen.getByDisplayValue(testEmailValid)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(testPasswordValid)).toBeInTheDocument();
 
     const button = screen.getByText("Login");
 
-    fireEvent.click(button);
+    user.click(button);
+
     await waitFor(() => {
-      expect(handleSubmit).toHaveBeenCalledTimes(0);
+      expect(mockedLogin).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test("render content from Login Component 2", async () => {
+    const { getByPlaceholderText } = render(<Login />);
+
+    fillLoginFormWithValidData(testEmailInvalid, testPasswordValid);
+
+    expect(screen.getByDisplayValue(testEmailInvalid)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(testPasswordValid)).toBeInTheDocument();
+
+    const button = screen.getByText("Login");
+
+    user.click(button);
+
+    expect(
+      await screen.findByRole("heading", {
+        name: /Please enter your email addres in format yourname@example.com/i,
+      })
+    ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockedLogin).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  test.only("error", async () => {
+    mockedLogin.mockRejectedValueOnce(
+      new Error("Firebase: Error (auth/user-not-found).")
+    );
+    render(<Login />);
+    fillLoginFormWithValidData(testEmailValid, testPasswordValid);
+
+    const button = screen.getByText("Login");
+    user.click(button);
+
+    expect(
+      await screen.findByRole("heading", {
+        name: /User not found./i,
+      })
+    ).toBeInTheDocument();
+  });
+
+  test("show error when form is not complete", async () => {
+    render(<Login />);
+    const button = screen.getByText("Login");
+    user.click(button);
+
+    expect(
+      await screen.findByRole("heading", {
+        name: /fields must be filled/i,
+      })
+    ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockedLogin).toHaveBeenCalledTimes(0);
     });
   });
 });
